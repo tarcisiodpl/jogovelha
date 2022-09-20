315eb569cd9d7cb33aafbf919523fecc80531ddc
Wesllen Leonardo de Lira Santos <wesllen.2020111052@unicap.br>
2022-09-20 13:49:27 +0000

    inicio do método para criar novos estados

--- Jogo da Velha/src/jogadores/JogadorCavaleiro.java
+++ Jogo da Velha/src/jogadores/JogadorCavaleiro.java
@@ -1,5 +1,9 @@
 package jogadores;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import jogo.Tabuleiro;
 
 public class JogadorCavaleiro extends Jogador{
@@ -12,6 +16,29 @@ public class JogadorCavaleiro extends Jogador{
     public int[] jogar(int[][] tabuleiro) {
         int[] jogada = new int[2];
 
+        switch(tabuleiro.length){
+            case 3:
+
+            break;
+
+            case 10:
+
+            break;
+
+            case 50:
+            
+
+            break;
+        }
+
+
+        //buscas cegas para poderem ser implementadas? largura, profundidade e peso;
+        //resgatar a ideia da atividade de Eng. Software II: criar os estados para a próxima jogada (no caso da busca cega, apenas)
+
+
+
+
+        //estratégia legado do 3x3
         if(tabuleiro[0][0] == -1){
             jogada[0] = 0;
             jogada[1] = 0;
@@ -51,9 +78,20 @@ public class JogadorCavaleiro extends Jogador{
             return jogada;
     }
     }
-   
         return null;
-
     }
     
+    //gerar os próximos estados possíveis
+    public Stream geradorDeEstados(int[][] tabuleiro){
+        List<Tabuleiro> estados = new ArrayList<Tabuleiro>();
+        Tabuleiro novoEstado = new Tabuleiro(tabuleiro.length);//é preciso clonar isso
+
+
+
+
+        return estados.stream();
+    }
+
+
+
 }
