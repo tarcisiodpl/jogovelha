a954912fac4f78a56e7232219497318a557ff0d1
Wesllen Leonardo de Lira Santos <wesllen.2020111052@unicap.br>
2022-09-20 13:15:19 +0000

    adição do jogador cavaleiro

---
+++ Jogo da Velha/src/jogadores/JogadorCavaleiro.java
@@ -0,0 +1,59 @@
+package jogadores;
+
+import jogo.Tabuleiro;
+
+public class jogadorCavaleiro extends Jogador{
+
+    public jogadorCavaleiro(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public int[] jogar(int[][] tabuleiro) {
+        int[] jogada = new int[2];
+
+        if(tabuleiro[0][0] == -1){
+            jogada[0] = 0;
+            jogada[1] = 0;
+            return jogada;
+        }else if(tabuleiro[0][2] == -1){
+            jogada[0] = 0;
+            jogada[1] = 2;
+            return jogada;
+        }else if(tabuleiro[2][0] == -1){
+            jogada[0] = 2;
+            jogada[1] = 0;
+            return jogada;
+        }else if(tabuleiro[2][2] == -1){
+            jogada[0] = 2;
+            jogada[1] = 2;
+            return jogada;
+        }else{
+            if(tabuleiro[0][1] == -1){
+                jogada[0] = 0;
+                jogada[1] = 1;
+                return jogada;
+            }else if(tabuleiro[1][0] == -1){
+                jogada[0] = 1;
+                jogada[1] = 0;
+                return jogada;
+            }else if(tabuleiro[1][1] == -1){
+                jogada[0] = 1;
+                jogada[1] = 1;
+                return jogada;
+            }else if(tabuleiro[1][2] == -1){
+                jogada[0] = 1;
+                jogada[1] = 2;
+                return jogada;
+        }else if(tabuleiro[2][1] == -1){
+            jogada[0] = 2;
+            jogada[1] = 1;
+            return jogada;
+    }
+    }
+   
+        return null;
+
+    }
+    
+}
