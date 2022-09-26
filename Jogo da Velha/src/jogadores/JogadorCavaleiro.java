2ac27e8c1a2afb003e6f38b92e453c282b6cfd12
Marcos Rigel <marcosrigel99@gmail.com>
2022-09-26 18:55:21 -0300

    nova Atualizacao

--- Jogo da Velha/src/jogadores/JogadorCavaleiro.java
+++ Jogo da Velha/src/jogadores/JogadorCavaleiro.java
@@ -12,49 +12,46 @@ public JogadorCavaleiro(String nome) {
     super(nome);
 }
 
-
-public int[] jogar(int[][] tabuleiro) {
-   int meuSimbolo = super.getSimbolo();
-   int[] minhaJogada = new int[2];
-   minhaJogada[0] = -1; minhaJogada[1] = -1;
+public int[] jogar(int[][] matrizTabuleiro) {
+   int[] jogadaPrincipal = new int[2];
+   jogadaPrincipal[0] = -1; jogadaPrincipal[1] = -1;
     int jogadaperfeita = -1000;
-   for (int i = 0; i < tabuleiro.length; i++){
-       for (int j = 0; j < tabuleiro.length; j++){
-            if (tabuleiro[i][j] == -1) {
-                tabuleiro[i][j] = this.getSimbolo();
-                int jogada = jogadorMiniMax(tabuleiro, 0, false, this.getSimbolo());
-                tabuleiro[i][j] = -1;
+   for (int i = 0; i < matrizTabuleiro.length; i++){
+       for (int j = 0; j < matrizTabuleiro.length; j++){
+            if (matrizTabuleiro[i][j] == -1) {
+                matrizTabuleiro[i][j] = this.getSimbolo();
+                int jogada = jogadorMiniMax(matrizTabuleiro, 0, false, this.getSimbolo());
+                matrizTabuleiro[i][j] = -1;
                     if(jogada>jogadaperfeita){
-                   minhaJogada[0] = i;
-                    minhaJogada[1] = j;
+                    jogadaPrincipal[0] = i;
+                    jogadaPrincipal[1] = j;
                     jogadaperfeita= jogada;
-                } 
-                
-               
+                }     
             }
-                }}
-    return minhaJogada;
+        }
+    }
+    return jogadaPrincipal;
 }
 
-public static int jogadorMiniMax(int tabuleiro[][], int profundidade, Boolean maximo, int simbolo){
-    if (tabuleiro.length > 3 && profundidade >0) {
+public static int jogadorMiniMax(int matrizTabuleiro[][], int profundidade, Boolean maximo, int simbolo){
+    if (matrizTabuleiro.length > 3 && profundidade >0) {
         return 0;
     }
-    int valor = verifyTabu(tabuleiro, simbolo);
+    int valor = verifyTabu(matrizTabuleiro, simbolo);
     if (valor == 10 || valor == -10) {
         return valor;
     }
-    if (existeMove(tabuleiro) == false) {
+    if (existeMove(matrizTabuleiro) == false) {
         return 0;
     }
     if (maximo == true) {
         int best = -1000;
-        for (int i = 0; i < tabuleiro.length; i++){
-            for (int j = 0; j < tabuleiro.length; j++){
-                if (tabuleiro[i][j] == -1) {
-                    tabuleiro[i][j] = simbolo;
-                    best = Math.max(best, jogadorMiniMax(tabuleiro, profundidade + 1, !maximo, simbolo));
-                    tabuleiro[i][j] = -1;
+        for (int i = 0; i < matrizTabuleiro.length; i++){
+            for (int j = 0; j < matrizTabuleiro.length; j++){
+                if (matrizTabuleiro[i][j] == -1) {
+                    matrizTabuleiro[i][j] = simbolo;
+                    best = Math.max(best, jogadorMiniMax(matrizTabuleiro, profundidade + 1, !maximo, simbolo));
+                    matrizTabuleiro[i][j] = -1;
                 }
             }
         }
@@ -62,12 +59,12 @@ public static int jogadorMiniMax(int tabuleiro[][], int profundidade, Boolean ma
     }
     else {
         int bestmove = 1000;
-        for (int i = 0; i < tabuleiro.length; i++){
-            for (int j = 0; j < tabuleiro.length; j++){
-                if (tabuleiro[i][j] == -1) {
-                    tabuleiro[i][j] = -1*(simbolo - 1);
-                    bestmove = Math.min(bestmove, jogadorMiniMax(tabuleiro, profundidade + 1, !maximo, simbolo));
-                    tabuleiro[i][j] = -1;
+        for (int i = 0; i < matrizTabuleiro.length; i++){
+            for (int j = 0; j < matrizTabuleiro.length; j++){
+                if (matrizTabuleiro[i][j] == -1) {
+                    matrizTabuleiro[i][j] = -1*(simbolo - 1);
+                    bestmove = Math.min(bestmove, jogadorMiniMax(matrizTabuleiro, profundidade + 1, !maximo, simbolo));
+                    matrizTabuleiro[i][j] = -1;
                 }
             }
         }
@@ -75,21 +72,21 @@ public static int jogadorMiniMax(int tabuleiro[][], int profundidade, Boolean ma
     }
 }
 
-public static int verifyTabu(int tabuleiro[][], int simbolo){
+public static int verifyTabu(int matrizTabuleiro[][], int simbolo){
     int ganhou = 0;
     int perdeu = 0;
-    for (int i = 0; i < tabuleiro.length; i++) {
-        for (int j = 0; j < tabuleiro.length; j++) {
-            if(tabuleiro[i][j] == simbolo){
+    for (int i = 0; i < matrizTabuleiro.length; i++) {
+        for (int j = 0; j < matrizTabuleiro.length; j++) {
+            if(matrizTabuleiro[i][j] == simbolo){
                 ganhou++;
             }
-            else if(tabuleiro[i][j] == -1*(simbolo - 1)){
+            else if(matrizTabuleiro[i][j] == -1*(simbolo - 1)){
                 perdeu++;
             }
         }
-        if (ganhou == tabuleiro.length) {
+        if (ganhou == matrizTabuleiro.length) {
             return +10;
-        } else if (perdeu == tabuleiro.length) {
+        } else if (perdeu == matrizTabuleiro.length) {
             return -10;
         }
         ganhou = 0;
@@ -97,18 +94,18 @@ public static int verifyTabu(int tabuleiro[][], int simbolo){
     }
     ganhou = 0;
     perdeu = 0;
-    for (int i = 0; i < tabuleiro.length; i++) {
-        for (int j = 0; j < tabuleiro.length; j++) {
-            if(tabuleiro[j][i] == simbolo){
+    for (int i = 0; i < matrizTabuleiro.length; i++) {
+        for (int j = 0; j < matrizTabuleiro.length; j++) {
+            if(matrizTabuleiro[j][i] == simbolo){
                 ganhou++;
             }
-            else if(tabuleiro[j][i] == -1*(simbolo - 1)){
+            else if(matrizTabuleiro[j][i] == -1*(simbolo - 1)){
                 perdeu++;
             }
         }
-        if (ganhou == tabuleiro.length) {
+        if (ganhou == matrizTabuleiro.length) {
             return +10;
-        } else if (perdeu == tabuleiro.length) {
+        } else if (perdeu == matrizTabuleiro.length) {
             return -10;
         }
         ganhou = 0;
@@ -116,49 +113,48 @@ public static int verifyTabu(int tabuleiro[][], int simbolo){
     }
     ganhou = 0;
     perdeu = 0;
-    for (int i = 0; i < tabuleiro.length; i++) {
-        if(tabuleiro[i][i] == simbolo){
+    for (int i = 0; i < matrizTabuleiro.length; i++) {
+        if(matrizTabuleiro[i][i] == simbolo){
             ganhou++;
         }
-        else if(tabuleiro[i][i] == -1*(simbolo - 1)){
+        else if(matrizTabuleiro[i][i] == -1*(simbolo - 1)){
             perdeu++;
         }
     }
-    if (ganhou == tabuleiro.length) {
+    if (ganhou == matrizTabuleiro.length) {
         return +10;
-    } else if (perdeu == tabuleiro.length) {
+    } else if (perdeu == matrizTabuleiro.length) {
         return -10;
     }
     ganhou = 0;
     perdeu = 0;
-    int indexMax = tabuleiro.length - 1;
+    int indexMax = matrizTabuleiro.length - 1;
     for (int i = 0; i <= indexMax; i++) {
-        if(tabuleiro[i][indexMax - i] == simbolo){
+        if(matrizTabuleiro[i][indexMax - i] == simbolo){
             ganhou++;
         }
-        else if(tabuleiro[i][indexMax - i] == -1*(simbolo - 1)){
+        else if(matrizTabuleiro[i][indexMax - i] == -1*(simbolo - 1)){
             perdeu++;
         }
     }
-    if (ganhou == tabuleiro.length) {
+    if (ganhou == matrizTabuleiro.length) {
         return +10;
-    } else if (perdeu == tabuleiro.length) {
+    } else if (perdeu == matrizTabuleiro.length) {
         return -10;
     }
     return 0;
 }
 
-public static Boolean existeMove(int tabuleiro[][]) {
-    for (int i = 0; i < tabuleiro.length; i++)
-        for (int j = 0; j < tabuleiro.length; j++)
-            if (tabuleiro[i][j] == -1) {
+public static Boolean existeMove(int matrizTabuleiro[][]) {
+    for (int i = 0; i < matrizTabuleiro.length; i++)
+        for (int j = 0; j < matrizTabuleiro.length; j++)
+            if (matrizTabuleiro[i][j] == -1) {
                 return true;
             }
     return false;
 }
 
-public int alphabeta(JogadorCavaleiro g, int alpha, int beta, char vez ){
-    // Se g for terminal, retona a heuristica
+public int alphabeta(JogadorCavaleiro g, int alpha, int beta, char vez ) {
     if(vez=='0'){
         char ganhador = 0;
         if(ganhador == 'x')
@@ -169,17 +165,11 @@ public int alphabeta(JogadorCavaleiro g, int alpha, int beta, char vez ){
             return 0;
         }
     }
-    // Se for a vez de MAX
     if(vez == 'x'){
-        // v = -inf
         int v = Integer.MIN_VALUE;
-        // Para cada no filho de g
         for(JogadorCavaleiro next : g.prox()){
-            // v = max(v, alphabeta(filho, alpha, beta, min)
             v = Integer.max(v, alphabeta(next, alpha, beta, 'o'));
-            // alpha = max(alpha, v)
             alpha = Integer.max(alpha, v);
-            // se beta <= alpha => poda em beta
             if(beta <= alpha){
                 break;
             }
@@ -187,15 +177,11 @@ public int alphabeta(JogadorCavaleiro g, int alpha, int beta, char vez ){
         return v;
     }
     else{
-        // v = inf
         int v = Integer.MAX_VALUE;
-        // Para cada filho de g
+
         for(JogadorCavaleiro next : g.prox() ){
-            // v = min (v, alphabeta(filho,alpha,beta,max))
             v = Integer.min(v, alphabeta(next, alpha, beta, 'x'));
-            // beta = min(beta,v)
             beta = Integer.min(beta, v);
-            // se beta <= alpha => poda em alpha
             if(beta <= alpha){
                 break;
             }
@@ -208,10 +194,4 @@ List<JogadorCavaleiro> prox() {
     List<JogadorCavaleiro> resp = null;
     return  resp;
 }
-    //gerar os próximos estados possíveis
-    public Stream geradorDeEstados(int[][] tabuleiro) {
-        List<Tabuleiro> estados = new ArrayList<Tabuleiro>();
-        Tabuleiro novoEstado = new Tabuleiro(tabuleiro.length);//é preciso clonar isso
-        return estados.stream();
-    }
 }
